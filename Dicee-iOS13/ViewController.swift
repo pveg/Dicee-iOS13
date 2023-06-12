29c735b3ad56845f2d5ebff4d0d468feb45e8efa
pveg <pveg@live.com>
2023-06-12 22:48:42 +0100

    Finished app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,18 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var diceImageView1: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         let dices = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")]
         
-        diceImageView1.image = dices[leftDiceNumber]
-        diceImageView2.image = dices[rightDiceNumber]
-        
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        diceImageView1.image = dices.randomElement()
+        diceImageView2.image = dices.randomElement()
     }
 
 }
