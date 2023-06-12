02bb22af5c69065a9550d42ebce13f32bb80ee34
pveg <pveg@live.com>
2023-06-12 21:55:50 +0100

    adds basic logic to changing when button is pressed

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let dices = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"), UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"), UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")]
+        
+        diceImageView1.image = dices[leftDiceNumber]
+        diceImageView2.image = dices[rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
     }
-
-
+    
 }
 
